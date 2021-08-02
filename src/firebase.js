ac287523d12aef9c06066c28402029268b6818e4
emilielr <43408175+emilielr@users.noreply.github.com>
2021-08-02 20:57:00 +0200

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD8HU9RWhHFz-4MQdojkZ8-cCkRIzdhB0k",
+  authDomain: "workout-tracker-3e1fa.firebaseapp.com",
+  projectId: "workout-tracker-3e1fa",
+  storageBucket: "workout-tracker-3e1fa.appspot.com",
+  messagingSenderId: "27617111077",
+  appId: "1:27617111077:web:de7dcbbd81ab5e5a26a3cc",
+  measurementId: "G-TNJ12B692W",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export default app;
