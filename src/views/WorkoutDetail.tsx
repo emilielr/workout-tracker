293991fca343aa121f05b609ea2454855962615a
emilielr <43408175+emilielr@users.noreply.github.com>
2021-08-04 13:52:55 +0200

    Detail workout in a table

--- src/views/WorkoutDetail.tsx
+++ src/views/WorkoutDetail.tsx
@@ -1,4 +1,12 @@
-import { Button } from "@material-ui/core";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
 import { Link, useLocation } from "react-router-dom";
 
 type LocationState = {
@@ -15,18 +23,36 @@ export const WorkoutDetail = () => {
         Tilbake
       </Button>
       <h1>{workout.workoutName}</h1>
-      <h2>
-        {workout.exercises.map((exercise: Exercise, index: number) => {
-          return (
-            <div className="exercise" key={index}>
-              <p>{exercise.name}</p>
-              <p>
-                {exercise.reps}x{exercise.sets} / {exercise.kg} kg
-              </p>
-            </div>
-          );
-        })}
-      </h2>
+      <TableContainer>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Øvelse</TableCell>
+              <TableCell align="right" className="sets">
+                Sett
+              </TableCell>
+              <TableCell align="right" className="reps">
+                Reps
+              </TableCell>
+              <TableCell align="right" className="kg">
+                Kg
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {workout.exercises.map((exercise: Exercise, index: number) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>{exercise.name}</TableCell>
+                  <TableCell align="right">{exercise.sets}</TableCell>
+                  <TableCell align="right">{exercise.reps}</TableCell>
+                  <TableCell align="right">{exercise.kg}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
