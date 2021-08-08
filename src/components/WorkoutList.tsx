f6c735f1e7999d0f5b6d3d8400a8147a9a5a0d17
emilielr <43408175+emilielr@users.noreply.github.com>
2021-08-08 12:19:29 +0200

    Pagination (#10)
    * Pagination working
    
    * order wokrouts in desc order
    
    * change to start after

--- src/components/WorkoutList.tsx
+++ src/components/WorkoutList.tsx
@@ -1,41 +1,138 @@
+import { TablePagination } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
-import { usePrevious } from "../utils/comparison";
 import { WorkoutCard } from "./WorkoutCard";
+import firebase from "firebase";
 
 export const WorkoutList = () => {
+  const [prevPage, setPrevPage] =
+    useState<firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData> | null>(
+      null
+    );
+  const [nextPage, setNextPage] =
+    useState<firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData> | null>(
+      null
+    );
+
+  const [workoutCount, setWorkoutCount] = useState(0);
   const [workouts, setWorkouts] = useState<Workout[]>([]);
-  const prevWorkouts = usePrevious(workouts);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const getWorkoutCount = () => {
+    let count = 0;
+    db.collection("workouts")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach(() => {
+          count++;
+        });
+        setWorkoutCount(count);
+      });
+  };
+
+  const getNextPaginatedWorkouts = () => {
+    db.collection("workouts")
+      .orderBy("date", "desc")
+      .startAfter(nextPage)
+      .limit(rowsPerPage)
+      .get()
+      .then((querySnapshot) => {
+        let temp: Workout[] = [];
+        querySnapshot.forEach((doc) => {
+          temp.push({
+            id: doc.id,
+            workoutName: doc.data().workoutName,
+            date: doc.data().date.toDate(),
+            exercises: doc.data().exercises,
+          });
+        });
+        if (temp.length > workoutCount) {
+          setPrevPage(querySnapshot.docs[0]);
+          setNextPage(querySnapshot.docs[querySnapshot.docs.length - 1]);
+        }
+        setWorkouts(temp);
+      });
+  };
+
+  const getPrevPaginatedWorkouts = () => {
+    db.collection("workouts")
+      .orderBy("date", "desc")
+      .startAt(prevPage)
+      .limit(rowsPerPage)
+      .get()
+      .then((querySnapshot) => {
+        let temp: Workout[] = [];
+        querySnapshot.forEach((doc) => {
+          temp.push({
+            id: doc.id,
+            workoutName: doc.data().workoutName,
+            date: doc.data().date.toDate(),
+            exercises: doc.data().exercises,
+          });
+        });
+        if (temp.length > workoutCount) {
+          setPrevPage(querySnapshot.docs[0]);
+          setNextPage(querySnapshot.docs[querySnapshot.docs.length - 1]);
+        }
+        setWorkouts(temp);
+      });
+  };
 
   useEffect(() => {
-    if (prevWorkouts === workouts) {
-      db.collection("workouts")
-        .get()
-        .then((querySnapshot) => {
-          let temp: Workout[] = [];
-          querySnapshot.forEach((doc) => {
-            temp.push({
-              id: doc.id,
-              workoutName: doc.data().workoutName,
-              date: doc.data().date.toDate(),
-              exercises: doc.data().exercises,
-            });
+    getWorkoutCount();
+    db.collection("workouts")
+      .orderBy("date", "desc")
+      .limit(rowsPerPage)
+      .get()
+      .then((querySnapshot) => {
+        let temp: Workout[] = [];
+        querySnapshot.forEach((doc) => {
+          temp.push({
+            id: doc.id,
+            workoutName: doc.data().workoutName,
+            date: doc.data().date.toDate(),
+            exercises: doc.data().exercises,
           });
-          temp
-            .sort((a, b) => {
-              return new Date(a.date).getTime() - new Date(b.date).getTime();
-            })
-            .reverse();
-          setWorkouts(temp);
         });
+        setPrevPage(querySnapshot.docs[0]);
+        setNextPage(querySnapshot.docs[querySnapshot.docs.length - 1]);
+        setWorkouts(temp);
+      });
+  }, [rowsPerPage]);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    if (newPage > page) {
+      getNextPaginatedWorkouts();
+    } else {
+      getPrevPaginatedWorkouts();
     }
-  }, [prevWorkouts, workouts]);
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
     <div className="workout-list">
       {workouts.map((workout: Workout, index: number) => {
         return <WorkoutCard workout={workout} key={index} />;
       })}
+      <TablePagination
+        component="div"
+        count={workoutCount}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </div>
   );
 };
